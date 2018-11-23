b8d2ea567116c7e38c8c9d393498ceadfe60153c
Rebecca B <39128621+planocodegirl@users.noreply.github.com>
2018-11-23 12:13:06 -0600

    For loop to get all keys
    Used an enhanced for loop to get all the keys and then all the capitals paired with states (keys)

--- StateCapitals.java
+++ StateCapitals.java
@@ -1,6 +1,7 @@
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
 
 /*
  * To change this license header, choose License Headers in Project Properties.
@@ -27,23 +28,33 @@ public class StateCapitals {
         capitals.put("Colorado", "Hartford");
         capitals.put("Connecticut", "Denver");
         
-        System.out.println(capitals.size());
+        //System.out.println(capitals.size());
         
         //called Colorado by its key, 
         //put in new capital for that state
         //calleed colorado again with the new capital
         String getColorado = capitals.get("Colorado");
-        System.out.println(getColorado);
+        //System.out.println(getColorado);
         capitals.put("Colorado","Denver");
         getColorado = capitals.get("Colorado");
-        System.out.println(getColorado);
+        //System.out.println(getColorado);
         
         //same process for connecticut
         String getConnect = capitals.get("Connecticut");
-        System.out.println(getConnect);
+        //System.out.println(getConnect);
         capitals.put("Connecticut", "Hartford");
         getConnect = capitals.get("Connecticut");
-        System.out.println(getConnect);
+        //System.out.println(getConnect);
+        
+        //in orderr to get the items in our map we call it out by their key
+        Set<String> keys = capitals.keySet();
+        //Enhanced for loop
+        //left side declare variable of the type that is in the set
+        // : right side we put the collection we want to pull out of this set
+        for ( String currentKey : keys) {
+            String currentCapital = capitals.get(currentKey);
+            System.out.println("The capital of "+currentKey+" is "+currentCapital+".");
+        }
         
     }
 }
