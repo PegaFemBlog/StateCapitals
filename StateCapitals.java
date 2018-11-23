c5db8847ef7d36668587085cfdf1c2317cdc1ff6
Rebecca B <39128621+planocodegirl@users.noreply.github.com>
2018-11-23 11:58:24 -0600

    Used keys to make corrections
    intentionally assigned wrong state capitals to two states
    used the .get and .put methods to reassign correct capitals to these states

--- StateCapitals.java
+++ StateCapitals.java
@@ -22,9 +22,28 @@ public class StateCapitals {
         capitals.put("Arizona", "Pheonix");
         capitals.put("Arkansas", "Little Rock");
         capitals.put("California", "Sacramento");
+        
+        //purposely assigned wrong capitals to these two entries
         capitals.put("Colorado", "Hartford");
         capitals.put("Connecticut", "Denver");
         
         System.out.println(capitals.size());
+        
+        //called Colorado by its key, 
+        //put in new capital for that state
+        //calleed colorado again with the new capital
+        String getColorado = capitals.get("Colorado");
+        System.out.println(getColorado);
+        capitals.put("Colorado","Denver");
+        getColorado = capitals.get("Colorado");
+        System.out.println(getColorado);
+        
+        //same process for connecticut
+        String getConnect = capitals.get("Connecticut");
+        System.out.println(getConnect);
+        capitals.put("Connecticut", "Hartford");
+        getConnect = capitals.get("Connecticut");
+        System.out.println(getConnect);
+        
     }
 }
